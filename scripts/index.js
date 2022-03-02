c4fa3dd713d9013e139c4ca7d2b9267b351b5160
Abraham Nnaji <nnajiabraham@gmail.com>
2022-03-01 20:01:53 -0600

    added filter functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,66 +1,122 @@
 const noteItem = {
-    id: Math.random().toString(),
-    title: "Our First Note",
-    noteBody: "This is exciting",
-    lastEditedDate: new Date(1644541279745) 
-}  
+  id: Math.random().toString(),
+  title: "Our First Note",
+  noteBody: "This is exciting",
+  lastEditedDate: new Date(1644541279745),
+};
 
-const noteList = [noteItem, noteItem, noteItem]
+const noteListData = [
+    {
+        id: Math.random().toString(),
+        title: "Our First Note",
+        noteBody: "This is exciting",
+        lastEditedDate: new Date(1644541279745),
+    },
+    {
+        id: Math.random().toString(),
+        title: "Our Second Note",
+        noteBody: "This is exciting",
+        lastEditedDate: new Date(),
+    },
+    {
+        id: Math.random().toString(),
+        title: "Our Third Note",
+        noteBody: "This is exciting",
+        lastEditedDate: new Date(1645576363789),
+    },
+    {
+        id: Math.random().toString(),
+        title: "Our Fourth Note",
+        noteBody: "This is exciting",
+        lastEditedDate: new Date(1645570363789),
+    },
+    {
+        id: Math.random().toString(),
+        title: "Our Fifth Note",
+        noteBody: "This is exciting",
+        lastEditedDate: new Date(),
+    },
+];
+
+const createNoteButton = document.querySelector(".create_note");
+const noteSection = document.querySelector(".note_section");
+const noNotesView = document.querySelector(".no_note");
 
 const generateLastEditedTime = (noteTime) => {
-    const timeNow = new Date();
+  const timeNow = new Date();
 
-    const timeDiffInMin = (timeNow.getTime() - noteTime.getTime()) / 1000 / 60;
+  const timeDiffInMin = (timeNow.getTime() - noteTime.getTime()) / 1000 / 60;
 
-    const roundedTime = Math.round(timeDiffInMin)
+  const roundedTime = Math.round(timeDiffInMin);
 
-    return roundedTime > 1 
-        ? `${roundedTime} minutes` 
-        : roundedTime == 1 
-            ? `${roundedTime} minute`
-            : "few seconds";
-}
+  return roundedTime > 1
+    ? `${roundedTime} minutes`
+    : roundedTime == 1
+    ? `${roundedTime} minute`
+    : "few seconds";
+};
 
 const generateNoteItemView = (note) => {
-    const noteElement = document.createElement("a")
-    const noteTitleElement = document.createElement("p")
-    const noteStatusElement = document.createElement("p")
+  const noteElement = document.createElement("a");
+  const noteTitleElement = document.createElement("p");
+  const noteStatusElement = document.createElement("p");
 
-    noteTitleElement.textContent = note.title
-    noteStatusElement.textContent = `Last edited ${generateLastEditedTime(note.lastEditedDate)} ago`
+  noteTitleElement.textContent = note.title;
+  noteStatusElement.textContent = `Last edited ${generateLastEditedTime(
+    note.lastEditedDate
+  )} ago`;
 
-    noteElement.setAttribute("href", "#")
-    noteElement.setAttribute("class", "note-item")
+  noteElement.setAttribute("href", "#");
+  noteElement.setAttribute("class", "note-item");
 
-    // noteElement.classList.add('dfdf')
-    noteElement.appendChild(noteTitleElement)
-    noteElement.appendChild(noteStatusElement)
+  noteElement.appendChild(noteTitleElement);
+  noteElement.appendChild(noteStatusElement);
 
-    return noteElement;
-} 
+  return noteElement;
+};
 
-function renderNotes () {
-    if(noteList.length <= 0){
-        return;
-    }
-    
-    const noNotesView = document.querySelector(".no_note")
-    noNotesView.remove()
+function renderNotes(noteList) {
+  noteSection.innerHTML = ''
+  noteSection.appendChild(createNoteButton)
 
-    const noteListItemView  = noteList.map(function (note){
-        const noteView = generateNoteItemView(note);
+  if (noteList.length <= 0) {
+    noteSection.insertBefore(noNotesView, createNoteButton);
+    return;
+  }
 
-        return noteView
-    })
+  if(noNotesView){
+    noNotesView.remove();
+  }
 
-    const createNoteButton = document.querySelector(".create_note")
-    const noteSection = document.querySelector(".note_section")
+  const noteListItemView = noteList.map(function (note) {
+    const noteView = generateNoteItemView(note);
+    return noteView;
+  });
 
-    noteListItemView.forEach(function (noteView) {
-        noteSection.insertBefore(noteView, createNoteButton)
-    })
+  noteListItemView.forEach(function (noteView) {
+    noteSection.insertBefore(noteView, createNoteButton);
+  });
 }
+
+const filterNoteSearchBar = document.querySelector(".filter-notes")
+
+const filterNotesHandler = (event) =>{
+  const searchInput = event.target.value
+  const filteredSearchNoteList = noteListData.filter( (note)=>{
+    return note.title.toLowerCase().includes(searchInput.toLowerCase())
+  })
+
+  renderNotes(filteredSearchNoteList)
+}
+
+const rightClickEventHandler = (e) => {
+  filterNoteSearchBar.value = '';
+  e.preventDefault()
+}
+
+filterNoteSearchBar.addEventListener('input', filterNotesHandler)
+filterNoteSearchBar.addEventListener('contextmenu', rightClickEventHandler)
 
 document.body.onload = () => {
-    renderNotes()
-}
+  renderNotes(noteListData);
+};
